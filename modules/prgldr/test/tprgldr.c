aa57530524652d53a26977e98a326f0fef229194
SanyaNya <mrsanek2012@yandex.ru>
2019-02-24 12:36:41 +0300

    Начал писать тест prgldr

---
+++ modules/prgldr/test/tprgldr.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include "..\prgldr.h"
+#include "tprgldr.h"
+
+const uint8_t testfile_data[7] = {0xb8,0x01,0x00, 0xb9,0x02,0x00, 0xc3};
+
+void Test_LoadProgram()
+{
+	
+}
+
+void Test_prgldr()
+{
+	
+}
+
+int main()
+{
+	
+	
+	return 0;
+}
---
+++ modules/prgldr/test/tprgldr.h
@@ -0,0 +1,10 @@
+#ifndef TPRGLDR_H
+#define TPRGLDR_H
+
+void Test_LoadProgram();
+
+void Test_prgldr();
+
+int main();
+
+#endif
